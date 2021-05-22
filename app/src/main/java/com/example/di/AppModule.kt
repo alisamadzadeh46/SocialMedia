77819e457e7ee48e6ca158214714396a4e8ab343
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-05-22 21:52:21 +0430

    create Application class and AppModule class

---
+++ app/src/main/java/com/example/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.example.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.socialmedia.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplicationContext(
+        @ApplicationContext context: Context
+    ) = context
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_error)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+
+}
