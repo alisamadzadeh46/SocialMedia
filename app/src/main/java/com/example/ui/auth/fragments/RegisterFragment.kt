8ce46dd83291091b8865e281efaca46e460eb3c4
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-05-23 14:51:10 +0430

    fix login and register ui

--- app/src/main/java/com/example/ui/auth/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/ui/auth/fragments/RegisterFragment.kt
@@ -1,10 +1,29 @@
 package com.example.ui.auth.fragments
 
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.socialmedia.R
+import kotlinx.android.synthetic.main.fragment_register.*
 
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        login.setOnClickListener {
+            if (findNavController().previousBackStackEntry != null) {
+                findNavController().popBackStack()
+            } else {
+                findNavController().navigate(
+                    RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+                )
+            }
+
+        }
+    }
 
 }
