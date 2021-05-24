a2c425b89294b0ba6aaa33093423ac5f84014889
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-05-24 11:07:44 +0430

    create menu

--- app/src/main/java/com/example/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/ui/activity/MainActivity.kt
@@ -1,15 +1,56 @@
 package com.example.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.socialmedia.R
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment)
+                as NavHostFragment
+        bottomNavigationView.apply {
+            background = null
+            menu.getItem(2).isEnabled = false
+            setupWithNavController(navHostFragment.findNavController())
+            setOnNavigationItemReselectedListener {
 
+            }
+        }
+        fabNewPost.setOnClickListener {
+            navHostFragment.findNavController().navigate(
+                R.id.globalActionToCreatePostFragment
+            )
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                FirebaseAuth.getInstance().signOut()
+                Intent(this, AuthActivity::class.java).also {
+                    startActivity(it)
+                }
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
