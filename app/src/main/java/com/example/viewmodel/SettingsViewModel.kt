7990a543742c6433e0e6278de1798402fe943ed5
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-05-27 19:29:11 +0430

    Build and complete  SettingsViewModel.kt

---
+++ app/src/main/java/com/example/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.viewmodel
+
+import android.content.Context
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.entities.ProfileUpdate
+import com.example.data.entities.User
+import com.example.repositories.impl.ProfileUpdateImpl
+import com.example.socialmedia.R
+import com.example.utils.Constants.MIN_USERNAME_LENGTH
+import com.example.utils.Event
+import com.example.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val profileUpdateImpl: ProfileUpdateImpl,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Main
+) : ViewModel() {
+    private val _updateProfileStatus = MutableLiveData<Event<Resource<Any>>>()
+    val updateProfileStatus: LiveData<Event<Resource<Any>>> = _updateProfileStatus
+
+    private val _getUserStatus = MutableLiveData<Event<Resource<User>>>()
+    val getUserStatus: LiveData<Event<Resource<User>>> = _getUserStatus
+
+    private val _curImageUri = MutableLiveData<Uri>()
+    val curImageUri: LiveData<Uri> = _curImageUri
+
+    fun updateProfile(applicationContext:Context,profileUpdate: ProfileUpdate) {
+        if(profileUpdate.username.isEmpty() || profileUpdate.description.isEmpty()) {
+            val error = applicationContext.getString(R.string.error_input_empty)
+            _updateProfileStatus.postValue(Event(Resource.Error(error)))
+        } else if(profileUpdate.username.length < MIN_USERNAME_LENGTH) {
+            val error = applicationContext.getString(R.string.error_username_too_short)
+            _updateProfileStatus.postValue(Event(Resource.Error(error)))
+        } else {
+            _updateProfileStatus.postValue(Event(Resource.Loading()))
+            viewModelScope.launch(dispatcher) {
+                val result = profileUpdateImpl.updateProfile(profileUpdate)
+                _updateProfileStatus.postValue(Event(result))
+            }
+        }
+    }
+
+    fun getUser(uid: String) {
+        _getUserStatus.postValue(Event(Resource.Loading()))
+        viewModelScope.launch(dispatcher) {
+            val result = profileUpdateImpl.user(uid)
+            _getUserStatus.postValue(Event(result))
+        }
+    }
+
+    fun setCurImageUri(uri: Uri) {
+        _curImageUri.postValue(uri)
+    }
+}
